c10d070d3172abe570a9a3c14f102d86b5afaf2c
Julieta Pontino <jpontino@altimetrik.com>
2024-01-29 11:27:30 -0300

    add cards container scrolling

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -8,13 +8,15 @@ export default async function Courses() {
     const courses = await api.getCourses();
 
     return (
-        <section className='p-6'>
+        <section className='p-6 h-full overflow-hidden'>
             <div className='flex justify-between items-center mb-6'>
                 <h1 className='text-3xl font-bold text-brand-black-100'>Cursos</h1>
                 <Button>Agregar curso</Button>
             </div>
 
-            <div className={`grid auto-rows-[190px] grid-cols-[repeat(auto-fit,minmax(250px,_1fr))] gap-4`}>
+            <div
+                className={`grid auto-rows-[190px] grid-cols-[repeat(auto-fit,minmax(250px,_1fr))] gap-4 overflow-scroll h-full no-scrollbar`}
+            >
                 {courses.map(({ _id, name, description, amount }) => (
                     <Card key={_id} className='waves shadow-sm'>
                         <CardHeader>
