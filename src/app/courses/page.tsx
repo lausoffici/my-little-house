e200d06ed45da2d6e5c952ef6974ba9088b07349
Lautaro Soffici <lautarosoffici@gmail.com>
2024-02-06 18:13:37 -0300

    remove unused div

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -14,25 +14,23 @@ export default async function Courses() {
                 <h1 className='text-3xl font-bold text-foreground'>Cursos</h1>
                 <AddCourseDialog />
             </div>
-            <div className=''>
-                <div
-                    className={`grid auto-rows-[fit-content(1em)] grid-cols-[repeat(auto-fit,minmax(250px,_1fr))] gap-4 overflow-scroll no-scrollbar`}
-                >
-                    {courses.map(({ _id, name, description, amount }) => (
-                        <Card key={_id} className='shadow-sm'>
-                            <CardHeader className='flex-row items-center justify-between relative'>
-                                <CardTitle className='w-4/5'>{name}</CardTitle>
-                                <CourseCardMenu course={{ _id, name, description, amount }} />
-                            </CardHeader>
-                            <CardContent>
-                                <p className='text-gray-600'>{description}</p>
-                                <Badge variant='secondary' className='px-1 mt-2 w-fit text-md'>
-                                    {formatCurrency(amount)}
-                                </Badge>
-                            </CardContent>
-                        </Card>
-                    ))}
-                </div>
+            <div
+                className={`grid auto-rows-[fit-content(1em)] grid-cols-[repeat(auto-fit,minmax(250px,_1fr))] gap-4 overflow-scroll no-scrollbar`}
+            >
+                {courses.map(({ _id, name, description, amount }) => (
+                    <Card key={_id} className='shadow-sm'>
+                        <CardHeader className='flex-row items-center justify-between relative'>
+                            <CardTitle className='w-4/5'>{name}</CardTitle>
+                            <CourseCardMenu course={{ _id, name, description, amount }} />
+                        </CardHeader>
+                        <CardContent>
+                            <p className='text-gray-600'>{description}</p>
+                            <Badge variant='secondary' className='px-1 mt-2 w-fit text-md'>
+                                {formatCurrency(amount)}
+                            </Badge>
+                        </CardContent>
+                    </Card>
+                ))}
             </div>
         </section>
     );
