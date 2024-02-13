f15adf7d25bc05d3b4ed87b8b6d10d8f92430311
Julieta Pontino <jpontino@altimetrik.com>
2024-02-13 19:54:11 -0300

    add student profile card

--- src/app/students/[id]/page.tsx
+++ src/app/students/[id]/page.tsx
@@ -1,11 +1,65 @@
+import { Pencil1Icon } from '@radix-ui/react-icons';
+
 import api from '@/api';
+import StudentDetail from '@/components/students/student-detail';
+import { Badge } from '@/components/ui/badge';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import { Label } from '@/components/ui/label';
+import { Separator } from '@/components/ui/separator';
 
 export default async function StudentPage({ params: { id } }: { params: { id: string } }) {
-    const student = await api.getStudent(id);
+    const { firstName, lastName, courses, email, description, address } = await api.getStudent(id);
+
+    const fullName = `${firstName} ${lastName}`;
 
     return (
-        <div>
-            <h1>{`${student.firstName} ${student.lastName}`}</h1>
+        <div className='flex flex-col gap-4'>
+            <div>
+                <h1 className='text-3xl font-bold text-foreground'>{fullName}</h1>
+                <p className='text-md text-foreground'>Detalles del estudiante</p>
+            </div>
+
+            <div className='flex flex-row gap-3'>
+                <Card className='w-2/4'>
+                    <CardHeader className='flex flex-row justify-between w-full'>
+                        <CardTitle> Información personal</CardTitle>
+                        <Button variant='outline' size='sm' title='Editar' style={{ marginTop: 0 }}>
+                            <Pencil1Icon className='mr-2' /> Editar
+                        </Button>
+                    </CardHeader>
+                    <CardContent>
+                        <div className='flex flex-col gap-3 justify-center'>
+                            <StudentDetail label='Nombre' info={firstName} />
+                            <Separator />
+                            <StudentDetail label='Apellido' info={lastName} />
+                            <Separator />
+                            <StudentDetail label='Dirección' info={address} />
+                            <Separator />
+                            <StudentDetail label='Email' info={email} />
+                            <Separator />
+                            <StudentDetail label='Descripción' info={description} />
+                            <Separator />
+                            <div className='flex items-center gap-2'>
+                                <Label className='text-xs'>Cursos </Label>
+                                <div>
+                                    {courses?.map((course) => (
+                                        <Badge variant='secondary' key={course} className='py-1 px-2 text-sm mr-2'>
+                                            {course}
+                                        </Badge>
+                                    ))}
+                                </div>
+                            </div>
+                        </div>
+                    </CardContent>
+                </Card>
+                <Card className='w-2/4'>
+                    <CardHeader>
+                        <CardTitle>Cuotas</CardTitle>
+                    </CardHeader>
+                    <CardContent></CardContent>
+                </Card>
+            </div>
         </div>
     );
 }
---
+++ src/components/students/student-detail.tsx
@@ -0,0 +1,15 @@
+import { Label } from '../ui/label';
+
+interface StudentDetailProps {
+    label: string;
+    info: string | undefined;
+}
+
+export default function StudentDetail({ label, info }: StudentDetailProps) {
+    return (
+        <div className='flex items-center gap-2'>
+            <Label className='text-xs'>{label}</Label>
+            <p className='py-1 px-2 text-sm'>{info}</p>
+        </div>
+    );
+}
