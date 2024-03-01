4fbf7e17fd305008b2f1dbe9fd4a03ed71544941
Julieta Pontino <jpontino@altimetrik.com>
2024-03-01 11:14:29 -0300

    fix receipt-badge width

--- src/components/receipts/receipt-badge.tsx
+++ src/components/receipts/receipt-badge.tsx
@@ -5,7 +5,7 @@ import { padWithZeros } from '@/lib/utils';
 
 export default function ReceiptBadge({ receiptId }: { receiptId: number }) {
     return (
-        <Badge className='flex gap-1 items-center w-[100px]'>
+        <Badge className='flex gap-1 items-center w-[min(100px,_100%)]'>
             <span>#{padWithZeros(receiptId)}</span> <ExternalLinkIcon />
         </Badge>
     );
