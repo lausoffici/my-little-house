083a4194f3ffa2dbc4d40ded2cfed7f885799ccf
Lautaro Soffici <lautarosoffici@gmail.com>
2024-05-31 16:27:29 -0300

    change getMonthName

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -40,12 +40,34 @@ export function capitalizeFirstLetter(string: string) {
 }
 
 export function getMonthName(monthNumber: number) {
-  const date = new Date();
-  date.setMonth(monthNumber - 1);
-
-  if (date.getMonth() <= 0) return '';
-
-  return capitalizeFirstLetter(date.toLocaleString('es', { month: 'long' }));
+  switch (monthNumber) {
+    case 1:
+      return 'Enero';
+    case 2:
+      return 'Febrero';
+    case 3:
+      return 'Marzo';
+    case 4:
+      return 'Abril';
+    case 5:
+      return 'Mayo';
+    case 6:
+      return 'Junio';
+    case 7:
+      return 'Julio';
+    case 8:
+      return 'Agosto';
+    case 9:
+      return 'Septiembre';
+    case 10:
+      return 'Octubre';
+    case 11:
+      return 'Noviembre';
+    case 12:
+      return 'Diciembre';
+    default:
+      return '--';
+  }
 }
 
 export const isToday = (date: Date) => {
