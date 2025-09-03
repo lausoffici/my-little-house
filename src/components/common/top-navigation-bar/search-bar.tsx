f625e2f43a5bc0c6e9897de5689c9c82514c250e
Julieta Pontino <julieta.pontino@gmail.com>
2025-09-03 15:29:39 -0300

    add invoice sort & remove inactive students search

--- src/components/common/top-navigation-bar/search-bar.tsx
+++ src/components/common/top-navigation-bar/search-bar.tsx
@@ -102,22 +102,15 @@ function DropdownContent({ studentNames, inputValue, isLoading, error }: Dropdow
   }
 
   if (studentNames.length > 0) {
-    return studentNames.map(({ id, firstName, lastName, active }) => (
+    return studentNames.map(({ id, firstName, lastName }) => (
       <Link
         key={id}
         href={`/students/${id}`}
         className='flex justify-between items-center text-sm font-medium bg-white hover:bg-gray-200 p-2 last:border-b-0 border-b'
       >
-        <div className='flex items-center gap-2'>
-          <span>
-            {firstName} {lastName}
-          </span>
-          {!active && (
-            <span className='inline-flex items-center px-1.5 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-800 dark:bg-gray-700 dark:text-gray-200'>
-              Inactivo
-            </span>
-          )}
-        </div>
+        <span>
+          {firstName} {lastName}
+        </span>
         <ArrowRightIcon />
       </Link>
     ));
