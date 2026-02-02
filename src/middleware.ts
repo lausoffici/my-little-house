44bec1d444298e91bbfee9c21304db97a6571a6d
Lautaro Soffici <lautarosoffici@gmail.com>
2026-02-02 13:09:12 -0300

    implement authentication middleware and NextAuth API route for user authentication

---
+++ src/middleware.ts
@@ -0,0 +1,11 @@
+import { withAuth } from 'next-auth/middleware';
+
+export default withAuth({
+  pages: {
+    signIn: '/api/auth/signin'
+  }
+});
+
+export const config = {
+  matcher: ['/((?!api/auth|_next/static|_next/image|favicon.ico|assets).*)']
+};
